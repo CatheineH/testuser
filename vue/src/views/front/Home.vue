a7bd4479d54fe39341cce78de977ec1ca533e2af
Catherine <3499347110@qq.com>
2023-02-28 12:16:34 +0800

    前台代码

---
+++ vue/src/views/front/Home.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div style="margin: 10px 0">
+      <el-carousel height="450px" :interval="10000">
+        <el-carousel-item v-for="item in imgs" :key="item">
+          <img :src="item" alt="" style="width: 100%">
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+
+    <div style="margin: 10px 0">
+      <el-row :gutter="10">
+        <el-col :span="6" v-for="item in files" :key="item.id" style="margin-bottom: 10px">
+
+          <div style="border: 1px solid #ccc; padding-bottom: 10px">
+            <img :src="item.url" alt="" style="width: 100%">
+            <div style="color: #666; padding: 10px">{{ item.name }}</div>
+            <div style="padding: 10px"><el-button type="primary">购买</el-button></div>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FrontHome",
+  data() {
+    return {
+      imgs: [
+          'https://img0.baidu.com/it/u=1524213542,4005275653&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500',
+          'https://img-qn.51miz.com/2017/05/13/06/2017051306556813_P867341_117b1bb3.png'
+      ],
+      files: []
+    }
+  },
+  created() {
+    this.request.get("/echarts/file/front/all").then(res => {
+      console.log(res.data)
+      this.files = res.data.filter(v => v.type === 'png' || v.type === 'jpg' || v.type === 'webp')
+    })
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
